76af8110ff917a61545224d4f8c2c1eefb4a877f
kantho202 <kanthochakma2025@gmail.com>
2026-02-18 23:38:37 +0600

    about-secton update

--- src/pages/Home/AboutSection/AboutSection.jsx
+++ src/pages/Home/AboutSection/AboutSection.jsx
@@ -153,7 +153,7 @@ const FloatingCard = styled.div`
     position: absolute;
     bottom: 20px;
     right: 20px;
-    background: white;
+    background: ;
     padding: 1.5rem;
     border-radius: 16px;
     box-shadow: 0 8px 24px rgba(0, 0, 0, 0.15);
