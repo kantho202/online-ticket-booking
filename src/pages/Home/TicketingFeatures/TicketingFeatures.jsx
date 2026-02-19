b614f4b228313251f80120cc53cd6599cb73a3c9
kantho202 <kanthochakma2025@gmail.com>
2026-02-19 21:58:21 +0600

    ticketfeatures update

--- src/pages/Home/TicketingFeatures/TicketingFeatures.jsx
+++ src/pages/Home/TicketingFeatures/TicketingFeatures.jsx
@@ -48,7 +48,7 @@ const TicketingFeatures = () => {
     ];
 
     return (
-        <div className="py-16 px-4 bg-white md:py-12">
+        <div className="py-16 px-4  md:py-12">
             <div className="max-w-7xl mx-auto">
                 <div className="text-center mb-12">
                     <h2 className="text-4xl md:text-3xl font-bold text-gray-800 mb-3">
